785117e299d5d515ef4a413703fa1e31dfefffe5
Akko-i <akikoishijima10@gmail.com>
2024-12-07 23:32:36 +1100

    init php files

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="container">
+        <div class="footer_flex">
+            <ul>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul>
+            <ul>
+                <li><a href="https://www.instagram.com/chic.style_boutique/" target="_blank"><img src="img/footer/icon-insta.png" alt="Instagram"></a></li>
+                <li><a href="https://www.facebook.com/chicstyleboutque/" target="_blank"><img src="img/footer/icon-fb.png" alt="Facebook"></a></li>
+            </ul>
+        </div>
+        <small>©2024 All Rights Reserved</small>
+    </div>
+</footer>
